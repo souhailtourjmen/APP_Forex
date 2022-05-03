65c5947cf215e674c68b27ce69aac6ed94d452fd
Tourjmen Souhail <95188930+souhailtourjmen@users.noreply.github.com>
2022-05-03 19:01:11 +0200

    correction erreur  niveaux scrapping

--- APP/helpers/test.js
+++ APP/helpers/test.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 
-function run(sql) {
-    let URL = "https://www.dinartunisien.com/fr/devise/"+sql;
+function run() {
+    let URL = "https://www.dinartunisien.com/fr/devise/bhd";
     return new Promise(async(resolve, reject) => {
         /* Initiate the Puppeteer browser */
         const browser = await puppeteer.launch();
@@ -18,30 +18,33 @@ function run(sql) {
             // if (!content) return { datas: [], meuiller: null, error: true };
 
             let datas = []
-            let meuiller = new Object();
+            let meuiller = {};
             meuiller.vente = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(1) > td:nth-child(3)")?.innerHTML;
             meuiller.imgbankV = document.querySelector("body > div:nth-child(6) > div:nth-child(4) > div.col-lg-4.col-md-4.col-sm-12.col-xs-12 > div > div:nth-child(2) > div:nth-child(2) > a > img")?.src;
             meuiller.achat = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(1) > td:nth-child(4)")?.innerHTML;
             meuiller.imgbankA = document.querySelector("body > div:nth-child(6) > div:nth-child(4) > div.col-lg-4.col-md-4.col-sm-12.col-xs-12 > div > div:nth-child(2) > div:nth-child(3) > a > img")?.src;
             let element = document.querySelectorAll(" tbody");
-            let i = 0;
+            
+            let i = 1;
             for (let ele of element) { /*3indi probleme  lehna lazmou yetrigel*/
-                let courdechange = new Object();
+                let courdechange = {};
                 courdechange.currency = "EUR";
-                courdechange.img_bank = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i + 2) + ") > td:nth-child(1) > a > img")?.src;
-                courdechange.date = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i + 2) + ") > td:nth-child(2) > span")?.innerText;
-                courdechange.vente = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i + 2) + ") > td:nth-child(3)")?.innerText;
-                courdechange.achat = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i + 2) + ") > td:nth-child(4)")?.innerText;
+                courdechange.img_bank = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i) + ") > td:nth-child(1) > a > img")?.src;
+                courdechange.date = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i) + ") > td:nth-child(2) > span")?.innerText;
+                courdechange.vente = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i) + ") > td:nth-child(3)")?.innerText;
+                courdechange.achat = document.querySelector("#banks-rates-tables > tbody > tr:nth-child(" + (i) + ") > td:nth-child(4)")?.innerText;
                 // courdechange.Date_Cours = document.querySelector("#main-content > div > div > table > tbody > tr:nth-child(" + (i + 2) + ") > td.date-change").innerText;
                 datas.push(courdechange);
                 i++;
             }
+      
             /* Returning an object filled with the scraped data */
             return { datas, meuiller }
         });
 
         /* Outputting what we scraped */
-         console.log(data);
+        console.log(data);
+        
         // fs.writeFile('./Stb_Bank.json', JSON.stringify(data), err => err ? console.log(err) : null);
         await browser.close();
         return resolve({ allResults: data.datas, bestResult: data.meuiller, error: null });
@@ -49,11 +52,8 @@ function run(sql) {
 
 }
 
-// run().then((data) => {
-//     console.log("fin code STB");
-// })
+run().then((data) => {
+    console.log("fin code STB");
+})
 
 // console.log(dat);
-module.exports = {
-    run
-}
