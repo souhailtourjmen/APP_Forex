701799dfb8c3744f7bc6a02914ab92fd680570b1
Tourjmen Souhail <95188930+souhailtourjmen@users.noreply.github.com>
2022-05-05 06:09:45 +0200

    meuiller devise

--- APP/controllers/index.js
+++ APP/controllers/index.js
@@ -1,4 +1,5 @@
 const Scrapper = require('../helpers/Scrapper.js');
+const nomdevise = require('../helpers/Devise.js');
 
 exports.index = function(req, res) {
     res.render('index.ejs', {}); // render the index.ejs file
@@ -8,8 +9,8 @@ exports.devise = async function(req, res) {
     try {
         const { currency } = req.params;
         const { allResults, bestResult } = await Scrapper.run(currency);
-
-        return res.render('Devise.ejs', { allResults, bestResult, currency });
+        let str= nomdevise.Nomdevise(currency);
+        return res.render('Devise.ejs', { allResults, bestResult,str });
     } catch (error) {
         console.log(error);
         return res.render('error.ejs', { message: "Oups", error });
--- APP/helpers/Devise.js
+++ APP/helpers/Devise.js
@@ -1,6 +1,173 @@
 function Nomdevise(devise) {
+    let content={};
     switch (devise){
-        case 'aed':
+        case 'aed':{
+            content.nom='Dirham Des Émirats Arabes Unis';
+            content.img='../images/flags/aed.png';
+            return content;
+        }
+        
+        case 'bhd' :{
+
+         
+            content.nom='Dinar Bahreïni';
+            content.img='../images/flags/bhd.png';
+            return content;
+        }
+        case 'cad' :{
+
           
+            content.nom='Dollar Canadien';
+            content.img='../images/flags/cad.png';
+            return content;
+        }
+         
+        
+        case 'chf':{
+
+       
+            content.nom='Franc Suisse';
+            content.img='../images/flags/chf.png';
+            return content;
+        }
+            
+        
+        case 'cny' :
+            {
+
+              
+                content.nom='Yuan Chinois';
+                content.img='../images/flags/cny.png';
+                return content;
+            }
+        
+        
+        case 'dkk' :{
+
+     
+            content.nom='Couronne Danoise';
+            content.img='../images/flags/dkk.png';
+            return content;
+        }
+         
+        
+        case 'dzd' :
+            {
+                
+                content.nom='Dinar Algérien';
+                content.img='../images/flags/dzd.png';
+                return content;
+            }
+           
+        
+        case 'eur' :
+            {
+            
+                content.nom='Euro';
+                content.img='../images/flags/eur.png';
+                return content;
+            }
+           
+        case 'gbp' :
+            {
+               
+                content.nom='Livre Sterling';
+                content.img='../images/flags/gbp.png';
+                return content;
+            }
+          
+        
+        case 'jpy' : {
+     
+            content.nom='Yen Japonais';
+            content.img='../images/flags/jpy.png';
+            return content;
+        }
+   
+        case 'kwd' : {
+      
+            content.nom='Dinar Koweitien';
+            content.img='../images/flags/kwd.png';
+            return content;
+        }
+        
+        case 'lyd' :
+            {
+          
+                content.nom='Dinar Libyen';
+                content.img='../images/flags/lyd.png';
+                return content;
+            }
+
+          
+        
+        case 'mad' :
+            {
+       
+                content.nom='Dirham Marocain';
+                content.img='../images/flags/mad.png';
+                return content;
+            }
+          
+       
+        case 'mro' :
+            {
+              
+                content.nom='Ouguiya Mauritanien';
+                content.img='../images/flags/mro.png';
+                return content;
+            }
+           
+     
+        case 'nok' :
+            {
+           
+                content.nom='Couronne Norvégienne';
+                content.img='../images/flags/nok.png';
+                return content;
+            }
+      
+     
+        case 'qar' :
+            {
+                
+                content.nom='Riyal Qatari';
+                content.img='../images/flags/qar.png';
+                return content;
+            }
+          
+   
+        case 'sar' :
+            {
+          
+                content.nom='Riyal Saoudien';
+                content.img='../images/flags/sar.png';
+                return content;
+            }
+          
+        
+        case 'sek' :
+            {
+               
+                content.nom='Couronne Suedoise';
+                content.img='../images/flags/sek.png';
+                return content;
+            }
+           
+        
+        case 'usd' :
+            {
+               
+                content.nom='Dollar Américain';
+                content.img='../images/flags/usd.png';
+                return content;
+            }
+           
+        
+
+
     }
+}
+module.exports = {
+    Nomdevise
 }
