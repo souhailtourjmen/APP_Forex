91873a2aa52ec742a766683bd0b4133121dcd518
Tourjmen Souhail <95188930+souhailtourjmen@users.noreply.github.com>
2022-05-07 08:19:45 +0200

    convertir

--- APP/public/Script/convertir.js
+++ APP/public/Script/convertir.js
@@ -2,14 +2,19 @@ function unite(str) {/*fonction retourne unite de devise et convertie en entier 
      str=parseFloat(str.substring(0,str.indexOf(" ")));
     return str;
 }
+function convertie(){
+//  let str=document
+//   .getElementById("etranger")
+//   .addEventListener("input", function(e) {
+//     document
+//       .getElementById("dinar")
+//       .value = e.target.value;
+// });
+let str=document.querySelector("#etranger").placeholder;
 
-    document
-    .getElementById("etranger")
-    .addEventListener("input", function(e) {
-      document
-        .getElementById("dinar")
-        .innerText = e.target.value;
-  });
+console.log(str);
+document.getElementById("dinar").value =unite(str);
+}
 
 
 // console.log(str);
