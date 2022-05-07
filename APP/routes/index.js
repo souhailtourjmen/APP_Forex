96b9ccd22fe283c414c762598f37ac998d982a0e
Tourjmen Souhail <95188930+souhailtourjmen@users.noreply.github.com>
2022-05-07 06:45:10 +0200

    tiit

--- APP/routes/index.js
+++ APP/routes/index.js
@@ -4,10 +4,6 @@ const controller = require('../controllers/index');
 
 /* GET home page. */
 
-// router.get('/devises/Banques', function(req, res){
-//     res.render('/Banque.html', {}); 
-
-// });
 router.get('/devises/:currency', controller.devise);
 
 router.get('/index.html', controller.index);
